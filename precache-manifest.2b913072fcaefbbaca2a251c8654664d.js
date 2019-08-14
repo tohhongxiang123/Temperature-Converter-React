70f158a33fc0828ed1a6367569c9b8e3387c9236
tohhongxiang123 <tohhongxiang@gmail.com>
2019-08-14 23:26:26 +0800

    Updates

---
+++ precache-manifest.2b913072fcaefbbaca2a251c8654664d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "962afb6a51815ed30cac9c1b4878e5c9",
+    "url": "/Temperature-Converter-React/index.html"
+  },
+  {
+    "revision": "de940ce65205572e0c3d",
+    "url": "/Temperature-Converter-React/static/css/main.4ecd1e49.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/Temperature-Converter-React/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "de940ce65205572e0c3d",
+    "url": "/Temperature-Converter-React/static/js/main.51383267.chunk.js"
+  },
+  {
+    "revision": "892a8c238441958c88bd",
+    "url": "/Temperature-Converter-React/static/js/runtime~main.d2a3d002.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/tohhongxiang123/Temperature-Converter-React/precache-manifest.79121ea3fe3ab0365f24e4e3e275df07.js"
+  "/Temperature-Converter-React/precache-manifest.2b913072fcaefbbaca2a251c8654664d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/tohhongxiang123/Temperature-Converter-React/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Temperature-Converter-React/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
