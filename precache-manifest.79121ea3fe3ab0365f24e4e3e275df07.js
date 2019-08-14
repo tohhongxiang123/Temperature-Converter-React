fc77d4aa10e54313f17e42acea4d080ea6904caa
tohhongxiang123 <tohhongxiang@gmail.com>
2019-08-14 23:17:03 +0800

    Updates

---
+++ precache-manifest.79121ea3fe3ab0365f24e4e3e275df07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af0ad05d667ba5d95af9221727856c48",
+    "url": "/tohhongxiang123/Temperature-Converter-React/index.html"
+  },
+  {
+    "revision": "df7ffde8e618bb234de2",
+    "url": "/tohhongxiang123/Temperature-Converter-React/static/css/main.4ecd1e49.chunk.css"
+  },
+  {
+    "revision": "5b537948f24afc95a481",
+    "url": "/tohhongxiang123/Temperature-Converter-React/static/js/2.5dbd8ed4.chunk.js"
+  },
+  {
+    "revision": "df7ffde8e618bb234de2",
+    "url": "/tohhongxiang123/Temperature-Converter-React/static/js/main.48d6635b.chunk.js"
+  },
+  {
+    "revision": "94875c064f75b81a938e",
+    "url": "/tohhongxiang123/Temperature-Converter-React/static/js/runtime~main.e76360c8.js"
+  }
+]);
